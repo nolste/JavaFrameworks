29909edcd385dc9cfd9e46482ca7fa88ac63a84b
nolste <nol.steiner@gmail.com>
2024-03-06 08:10:19 -0600

    adds industry-appropriate security features. SQL injection protection via parameterized query

--- src/main/java/com/example/demo/repositories/PartRepository.java
+++ src/main/java/com/example/demo/repositories/PartRepository.java
@@ -15,7 +15,9 @@ import java.util.List;
  */
 //This showcases the repository connection to the database with custom SQL query.
 //crud repository handles thee add, modify, and delete functions
+
 public interface PartRepository extends CrudRepository <Part,Long> {
+    //the question mark in this is a parameterized query. This parameterized query prevents SQL injections
     @Query("SELECT p FROM Part p WHERE p.name LIKE %?1%")
     public List<Part> search(String keyword);
 }
